2b761e0dc2b0ac63579e1c44ef06b46665fa5ec5
Oskar Niburski <oskarniburski@gmail.com>
2016-08-25 17:34:01 -0400

    Factory girl implementation

--- spec/models/battle_spec.rb
+++ spec/models/battle_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Battle, type: :model do
+  let(:battle) { build :battle }
+
   it "should return true" do 
     Battle.new.should_not be_valid
+  end
+
+  it 'status should be pending' do 
+    expect(battle.status).to eq('pending')
   end
 end
